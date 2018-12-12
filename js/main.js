ed4b7bcd9a5d4c3eb1e50fa2643fbd0c7e9b8ccf
taha yasaghi <yasaghy222@gmail.com>
2018-12-12 19:18:12 +0330

    Add project files.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var elements = document.getElementsByTagName("INPUT");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].oninvalid = function (e) {
+            e.target.setCustomValidity("");
+            if (!e.target.validity.valid) {
+                e.target.setCustomValidity("این فیلد نمی تواند خالی باشد");
+            }
+        };
+        elements[i].oninput = function (e) {
+            e.target.setCustomValidity("");
+        };
+    }
+})
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var topoffset = $(window).scrollTop(),
+            size = $(window).width(),
+            nav = $("nav");
+        if (topoffset > 85 && size > 1024) {
+            nav.addClass('fixed');
+        }
+        else if (size > 1024) {
+            nav.removeClass('fixed');
+        }
+    });
+    $('.button-collapse').sideNav({
+        menuWidth: 240,
+        edge: 'right',
+        closeOnClick: true,
+        draggable: false
+    });
+});
+
+
+
+
